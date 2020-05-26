f4293896075d7962912aac61353947b4b6dfa11d
dev-autodidacta <jdevunix@gmail.com>
2020-05-26 10:17:13 +0200

    agregando context y post ficticios

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'jdalmau',
+        'title': 'Blog post numero 1',
+        'content': 'Primer contenido en este super blog.',
+        'date_posted': '26 de Mayo, 2020',
+    },
+    {
+        'author': 'pepeMarismeño',
+        'title': 'Blog post numero 2',
+        'content': 'Segundo contenido en este super blog.',
+        'date_posted': '23 de Mayo, 2020',
+    },
+    {
+        'author': 'jdalmau33',
+        'title': 'Blog post numero 3',
+        'content': 'Tercero contenido en este super blog.',
+        'date_posted': '22 de Mayo, 2020',
+    }
+]
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {'posts':posts}
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title':'About'})
