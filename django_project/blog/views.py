c3c62fc6c758b50120a033ddd5264721439f46ee
dev-autodidacta <jdevunix@gmail.com>
2020-05-27 12:55:24 +0200

    eliminamos dummy data

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,5 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'author': 'jdalmau',
-        'title': 'Blog post numero 1',
-        'content': 'Primer contenido en este super blog.',
-        'date_posted': '26 de Mayo, 2020',
-    },
-    {
-        'author': 'pepeMarismeño',
-        'title': 'Blog post numero 2',
-        'content': 'Segundo contenido en este super blog.',
-        'date_posted': '23 de Mayo, 2020',
-    },
-    {
-        'author': 'jdalmau33',
-        'title': 'Blog post numero 3',
-        'content': 'Tercero contenido en este super blog.',
-        'date_posted': '22 de Mayo, 2020',
-    }
-]
-
 
 def home(request):
     context = {'posts':posts}
