cfc2a0215075bee8b6769bb9468e10635f8d6883
dev-autodidacta <jdevunix@gmail.com>
2020-05-27 13:38:26 +0200

    add content to blog from db

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def home(request):
-    context = {'posts':posts}
+    context = {'posts': Post.objects.all() }
     return render(request, 'blog/home.html', context)
 
 
